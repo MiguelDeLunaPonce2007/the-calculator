7c96624c93dbc1222ba301e5835588b2c744b106
MiguelDeLunaPonce2007 <64348087+MiguelDeLunaPonce2007@users.noreply.github.com>
2020-04-29 16:10:20 -0500

    Add files via upload

---
+++ input de numeros enteros.c
@@ -0,0 +1,20 @@
+//Enteros
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int numero1= 0;
+    int numero2 = 0;
+    int resta = 0;
+    printf("introduce numero 1: ");
+    scanf("%d", &numero1);
+    printf("introduce numero 2: ");
+    scanf("%d", &numero2);
+    resta = numero1 - numero2;
+    printf("La resta es: %d ",resta);
+    return 0;
+
+
+}
